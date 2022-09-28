d380201b47b8f2debdfa0c73cc2727bab08d18b0
Anthony Francis <tonytrack@gmail.com>
2022-09-28 23:10:53 +0100

    added google auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#index"
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   get "agenda" => "pages#agenda"
 
